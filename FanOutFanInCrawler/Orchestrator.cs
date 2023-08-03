fc99cadb8442ddf6ae27303a82d1ae694e890e10
tiemobang <48446367+tiemobang@users.noreply.github.com>
2023-08-03 09:47:59 -0700

    Fix explanation of Orchestrator.cs
    Line 61 was supposed to explain that we are skipping synchronization via `await` to get parallel execution of functions.
    Also added explanation of batch execution and fixed wording.

--- FanOutFanInCrawler/Orchestrator.cs
+++ FanOutFanInCrawler/Orchestrator.cs
@@ -15,7 +15,7 @@ namespace FanOutFanInCrawler
 {
     public static class Orchestrator
     {
-        // this needs to be configured when creating your Azure Function
+        // This needs to be configured when creating your Azure Function
         // with the portal:
         // https://docs.microsoft.com/en-us/azure/azure-functions/functions-how-to-use-azure-function-app-settings
         // command line:
@@ -49,17 +49,17 @@ namespace FanOutFanInCrawler
         public static async Task<string> RunOrchestrator(
             [OrchestrationTrigger] IDurableOrchestrationContext context)
         {
-            // retrieves the organization name from the Orchestrator_HttpStart function
+            // Retrieves the organization name from the Orchestrator_HttpStart function
             var organizationName = context.GetInput<string>();
-            // retrieves the list of repositories for an organization by invoking a separate Activity Function.
+            // Retrieves the list of repositories for an organization by invoking a separate Activity Function.
             var repositories = await context.CallActivityAsync<List<(long id, string name)>>("GetAllRepositoriesForOrganization", organizationName);
 
-            // Creates an array of task to store the result of each functions
+            // Creates an array of tasks to store the result of each function invokation
             var tasks = new Task<(long id, int openedIssues, string name)>[repositories.Count];
             for (int i = 0; i < repositories.Count; i++)
             {
-                // Starting a `GetOpenedIssues` activity WITHOUT `async`
-                // This will starts Activity Functions in parallel instead of sequentially.
+                // Starting a `GetOpenedIssues` activity WITHOUT waiting for the result via `await`
+                // This starts Activity Functions in parallel instead of sequentially.
                 tasks[i] = context.CallActivityAsync<(long, int, string)>("GetOpenedIssues", (repositories[i]));
             }
 
@@ -78,9 +78,9 @@ namespace FanOutFanInCrawler
         [FunctionName("GetAllRepositoriesForOrganization")]
         public static async Task<List<(long id, string name)>> GetAllRepositoriesForOrganization([ActivityTrigger] IDurableActivityContext context)
         {
-            // retrieves the organization name from the Orchestrator function
+            // Retrieve the organization name from the Orchestrator function
             var organizationName = context.GetInput<string>();
-            // invoke the API to retrieve the list of repositories of a specific organization
+            // Invoke the API to retrieve the list of repositories of a specific organization
             var repositories = (await github.Repository.GetAllForOrg(organizationName)).Select(x => (x.Id, x.Name)).ToList();
             return repositories;
         }
@@ -88,34 +88,35 @@ namespace FanOutFanInCrawler
         [FunctionName("GetOpenedIssues")]
         public static async Task<(long id, int openedIssues, string name)> GetOpenedIssues([ActivityTrigger] IDurableActivityContext context)
         {
-            // retrieve a tuple of repositoryId and repository name from the Orchestrator function
+            // Retrieve a tuple of repositoryId and repository name from the Orchestrator function
             var parameters = context.GetInput<(long id, string name)>();
 
-            // retrieves a list of issues from a specific repository
+            // Retrieve a list of issues from a specific repository
             var issues = (await github.Issue.GetAllForRepository(parameters.id)).ToList();
 
-            // returns a tuple of the count of opened issues for a specific repository
+            // Return a tuple of the count of opened issues for a specific repository
             return (parameters.id, issues.Count(x => x.State == ItemState.Open), parameters.name);
         }
 
         [FunctionName("SaveRepositories")]
         public static async Task SaveRepositories([ActivityTrigger] IDurableActivityContext context)
         {
-            // retrieves a tuple from the Orchestrator function
+            // Retrieve the list of repositories (tuples of repo. id, number of opened issues, and repo. name)
+            // from the Orchestrator function
             var parameters = context.GetInput<List<(long id, int openedIssues, string name)>>();
 
-            // create the client and table reference for Blob Storage
+            // Create the client and table reference for Blob Storage
             var client = account.CreateCloudTableClient();
             var table = client.GetTableReference("Repositories");
 
-            // create the table if it doesn't exist already.
+            // Create the table if it doesn't exist already.
             await table.CreateIfNotExistsAsync();
 
-            // creates a batch of operation to be executed
+            // Create a batch of operation to be executed
             var batchOperation = new TableBatchOperation();
             foreach (var parameter in parameters)
             {
-                // Creates an operation to add the repository to Table Storage
+                // Create an operation to add the repository to Table Storage
                 batchOperation.Add(TableOperation.InsertOrMerge(new Repository(parameter.id)
                 {
                     OpenedIssues = parameter.openedIssues,
@@ -123,6 +124,7 @@ namespace FanOutFanInCrawler
                 }));
             }
 
+            // Invoke whole batch of operations at once and wait for completion
             await table.ExecuteBatchAsync(batchOperation);
         }
 
